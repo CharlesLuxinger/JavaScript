e50834e6170bd060e9c82f996ca7480aab2b321b
CharlesLuxinger <charles.luxinger@spassu.com>
2019-08-23 11:38:30 -0300

    3. Condicionais

---
+++ conceitos-basicos/condicionais.js
@@ -0,0 +1,20 @@
+//If-else
+if (x != 1) {
+    
+} else if (x == 1) {
+    
+} else {
+    
+}
+
+(x == 1 ? "ok" : "fail");
+
+//Switch
+switch (x) {
+    case 1:
+        
+        break;
+
+    default:
+        break;
+}
