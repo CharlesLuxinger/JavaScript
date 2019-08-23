ab0d7f801a13d1863d4e3741a9b0654ecb42e098
CharlesLuxinger <charles.luxinger@spassu.com>
2019-08-23 14:42:55 -0300

    11. Arrays

---
+++ objetos/arrays.js
@@ -0,0 +1,17 @@
+var stark = ["Ned", "Jon", "Rob"];
+
+console.log(stark[0]);
+
+for (let index = 0; index < stark.length; index++) {
+    console.log(stark[index]);
+}
+
+stark.forEach(element => {
+    console.log(element);
+});
+
+var grupos = [
+    ["Ned", "Jon", "Rob"],
+    ["João", "José", "Juca"]
+    ["Maria", "Mariana", "Matilde"]  
+];
