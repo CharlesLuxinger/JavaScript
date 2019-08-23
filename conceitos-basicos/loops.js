709892fc0aa218660e1a010c6614aceea0a6f54e
CharlesLuxinger <charles.luxinger@spassu.com>
2019-08-23 11:51:28 -0300

    4. Loops

--- conceitos-basicos/loops.js
+++ conceitos-basicos/loops.js
@@ -3,6 +3,17 @@ for (var i = 0; i < 5; i++) {
     // Will execute 5 times
 }
 
+//For in
+var obj = {nome: 'João', idade: '31'};
+
+for (var prop in obj) {
+  //Exibe a chave do obj
+  console.log(prop);
+
+  //Exibe o valor do obj
+  console.log(obj[prop]);
+}
+
 // WHile
 while (condition) {
     rotina
