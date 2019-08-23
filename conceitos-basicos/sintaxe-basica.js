2a2a9ad27feeb5c22314ae09034d6b5ea4eefa59
CharlesLuxinger <charles.luxinger@spassu.com>
2019-08-23 11:20:02 -0300

    1. Sintaxe básica

---
+++ conceitos-basicos/sintaxe-basica.js
@@ -0,0 +1,28 @@
+//Todos os statements finalizam com ;
+var x = 15;
+
+//Valores: literais e variáveis
+//Literais
+
+10;
+10.5;
+
+"Olá mundo";
+'Olá mundo';
+
+//Variáveis
+var x;
+
+// Operadores
+
+//Operador de agregação
+x = 10;
+
+//Operador Aritméticos
+1 + 1 - 1 * 1 / 1;
+
+// Indentificadores possíveis
+var sintaxeBasica;
+var sintaxeBasica2;
+var _sintaxeBasica;
+var $sintaxeBasica;
