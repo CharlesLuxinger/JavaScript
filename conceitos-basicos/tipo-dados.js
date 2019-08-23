55fe4d1318667c553af3c001cc656d9b2d41b636
CharlesLuxinger <charles.luxinger@spassu.com>
2019-08-23 11:28:03 -0300

    2. Tipos de dados

---
+++ conceitos-basicos/tipo-dados.js
@@ -0,0 +1,26 @@
+//Number
+10;
+10.5;
+
+//String
+"Olá mundo";
+'Olá mundo';
+
+//boolean
+true;
+false;
+
+//Arrays
+[10, 11, 12];
+[10, 'Olá', true];
+
+//Objects
+var obj = {nome: 'João', idade: '31'};
+
+//Tipagem Dinâmica
+var x = 15;
+x = 'Olá';
+
+//Outros Valores
+var z; //undefined;
+var pessoa = null; //null;
